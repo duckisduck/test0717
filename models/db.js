143f6eb29c3d64b1472635b7570895373a2f51a8
duckisduck <1433822301@qq.com>
2017-07-17 18:11:58 +0800

    first class

---
+++ models/db.js
@@ -0,0 +1,33 @@
+var mongoose = require('mongoose');
+var db = mongoose.connect('mongodb://localhost/test');
+
+db.connection.on('error', function (error) { //监听是否有异常
+    console.log("Connection error");
+});
+db.connection.once('open', function () { //监听一次打开
+    //在这里创建你的模式和模型
+    console.log('connected!');
+});
+
+var userSchema = new mongoose.Schema({
+	name: String,
+	password: String
+});
+
+var user = db.model('user',userSchema);
+exports.usermodel = user;
+
+var tt = new mongoose.Schema({name:String});
+var task = new mongoose.Schema({contractname:String,type:String,Ssupervise:String,weight:Number,point:Number});
+var taskSchema = new mongoose.Schema({
+	name:String,
+	supervise:String,
+	kaohetime:Date,
+	updatetime:Date,
+	tasks:[task],
+	temptask:[tt]
+
+});
+
+var taskdb = db.model('taskdb',taskSchema);
+exports.taskmodel = taskdb;
---
+++ routes/index.js
@@ -0,0 +1,68 @@
+var express = require('express');
+var router = express.Router();
+var usermodel =require('../models/db').usermodel;
+var taskmodel =require('../models/db').taskmodel;
+/* GET home page. */
+router.get('/', function(req, res, next) {
+  res.render('index', { title: 'Express' });
+});
+
+
+
+router.post('/ucenter',function(req,res){
+	//var query_par = {name:req.body.name};
+	var newuser = {name:req.body.name,password:req.body.password};
+	usermodel.count(newuser,function(err,doc){
+		if(doc>=1){
+			console.log("success");
+		}else{
+			console.log("failed");
+		}
+		res.redirect('/');
+	});
+
+});
+
+router.post('/reg',function(req,res){
+	var query_par = {name:req.body.name};
+	var newuser =  new usermodel({
+		name:req.body.name,
+		password:req.body.password});	
+	usermodel.count(query_par,function(err,doc){
+		if (doc>=1) {
+			console.log('have been registered');
+			res.redirect('/');
+		}else{
+			console.log('success registered');
+			newuser.save(function(){
+				// if(err){
+				// 	console.log('save failed');
+				// }
+			})
+			res.redirect('/');
+		}
+	});
+});
+
+router.post('/update',function(req,res){
+	var task1 = {contractname:"aa",type:"bb",Ssupervise:"cc",weight:121,point:123};
+	var task2 = {contractname:"aa",type:"bb",Ssupervise:"cc",weight:121,point:123};
+	var tt1 = {name:"ehee"};
+	var tt2 = {name:"ioi"};
+	var newtask = new taskmodel({
+		name:req.body.name,
+		supervise:req.body.password,
+		kaohetime:Date(),
+		tasks:[task1,task2],
+		temptask:[tt1,tt2],
+		updatetime:Date()
+	});
+	newtask.save(function(){
+
+	})
+	res.redirect('/');
+});
+
+
+// err,newuser,numAffected
+module.exports = router;
